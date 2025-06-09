d60767a5a132cedbb5b538a1cf5ff55d5c411612
Abbas Aga <garderappstore@gmail.com>
2025-06-09 04:05:21 +0300

    upload 2

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -1,23 +1,22 @@
-// app/api/upload/route.ts
 import { NextRequest, NextResponse } from 'next/server'
 import { supabaseAdmin } from '@/lib/supabase/client'
 
 export async function POST(req: NextRequest) {
   const formData = await req.formData()
-  const file: File | null = formData.get('file') as File
+  const file = formData.get('file') as Blob | null
 
   if (!file) {
     return NextResponse.json({ error: 'No file provided' }, { status: 400 })
   }
 
   const buffer = Buffer.from(await file.arrayBuffer())
-  const sanitizedFileName = file.name.replace(/[^a-zA-Z0-9._-]/g, '_')
+  const sanitizedFileName = 'uploaded_file.png' // İsteğe göre
   const fileName = `${Date.now()}_${sanitizedFileName}`
 
   const { error } = await supabaseAdmin.storage
     .from('neosaas')
     .upload(`public/${fileName}`, buffer, {
-      contentType: file.type,
+      contentType: 'image/png',
       upsert: true,
       cacheControl: '3600',
     })
